e540f5cc37738fc1b5ca1d9e2eb9ee12f3f87316
Nestor Recinos <00083120@UCA.EDU.SV>
2023-08-16 00:37:55 -0600

    :wrench: Add database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModuleOptions } from '@Config/module';
+import { MongoConnection } from '@Config/database';
 
 @Module({
-  imports: [ConfigModuleOptions],
+  imports: [ConfigModuleOptions, MongoConnection],
   controllers: [AppController],
   providers: [AppService],
 })
