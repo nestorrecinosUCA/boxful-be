5911905e6f0c89d0d233d2002801cb184ebd1da1
Nestor Recinos <00083120@UCA.EDU.SV>
2023-08-16 09:54:02 -0600

    :memo: Update documentation

--- src/states/controllers/states.controller.ts
+++ src/states/controllers/states.controller.ts
@@ -1,15 +1,25 @@
 import { Controller, Get } from '@nestjs/common';
 import { StatesService } from '../services/states.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('States')
 @Controller('states')
 export class StatesController {
   constructor(private readonly statesService: StatesService) {}
 
+  @ApiOperation({
+    description: 'Find all states',
+    summary: 'Use it to find all states',
+  })
   @Get()
   async findAll() {
     return await this.statesService.findAll();
   }
 
+  @ApiOperation({
+    description: 'Poblate states',
+    summary: 'Use it to seed states',
+  })
   @Get('poblate')
   async poblate() {
     return this.statesService.poblate();
