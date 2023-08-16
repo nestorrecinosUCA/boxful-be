84dbee1daca36428726b6c2c0f56b9efab9c7d4c
Nestor Recinos <00083120@UCA.EDU.SV>
2023-08-16 00:54:26 -0600

    :wrench: Set up Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import configuration from '@Config/configuration';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Boxful API')
+    .setDescription('Technical assessment')
+    .setVersion('1.0')
+    .addTag('assessment')
+    .build();
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, swaggerDocument);
+
+  const { port } = configuration();
+  await app.listen(port);
+  console.log(`Server running on port ${port}`);
 }
 bootstrap();
