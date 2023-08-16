e4f8dd29c12aef68bb8ad794f72fa9b76ffedbdd
Nestor Recinos <00083120@UCA.EDU.SV>
2023-08-16 10:46:47 -0600

    :construction: Add schema for order

--- src/orders/schemas/order.schema.ts
+++ src/orders/schemas/order.schema.ts
@@ -1,1 +1,42 @@
-export class Order {}
+import { PackageType } from '@Orders/types';
+import { Prop, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type OrderDocument = HydratedDocument<Order>;
+
+export class Order {
+  @Prop()
+  collectingAddres: string;
+
+  @Prop()
+  date: Date;
+
+  @Prop()
+  names: string;
+
+  @Prop()
+  lastnames: string;
+
+  @Prop()
+  email: string;
+
+  @Prop()
+  userAddres: string;
+
+  @Prop()
+  state: string;
+
+  @Prop()
+  city: string;
+
+  @Prop()
+  reference: string;
+
+  @Prop()
+  indications: string;
+
+  @Prop()
+  packages: PackageType[];
+}
+
+export const OrderSchema = SchemaFactory.createForClass(Order);
