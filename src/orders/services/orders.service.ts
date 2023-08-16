beb66e1aaee3f83692c0f0a9fcf5bbb5a1b9236b
Nestor Recinos <00083120@UCA.EDU.SV>
2023-08-16 10:34:37 -0600

    :fire: Remove unnecessary methods

--- src/orders/services/orders.service.ts
+++ src/orders/services/orders.service.ts
@@ -7,20 +7,4 @@ export class OrdersService {
   create(createOrderDto: CreateOrderDto) {
     return 'This action adds a new order';
   }
-
-  findAll() {
-    return `This action returns all orders`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} order`;
-  }
-
-  update(id: number, updateOrderDto: UpdateOrderDto) {
-    return `This action updates a #${id} order`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} order`;
-  }
 }
